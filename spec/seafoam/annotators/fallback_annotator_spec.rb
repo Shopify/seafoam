b46e16e08373b6afb507f94105f393a43fe6a397
Chris Seaton <chris.seaton@shopify.com>
2019-11-02 14:30:16 -0400

    Spec the fallback annotator

---
+++ spec/seafoam/annotators/fallback_annotator_spec.rb
@@ -0,0 +1,33 @@
+require 'seafoam'
+
+require 'rspec'
+
+describe Seafoam::Annotators::FallbackAnnotator do
+  it 'always applies' do
+    expect(Seafoam::Annotators::FallbackAnnotator.applies?(Seafoam::Graph.new)).to be true
+  end
+
+  it 'adds a label annotation when there is a label property' do
+    graph = Seafoam::Graph.new
+    node = graph.create_node(0, {'label' => 'foo'})
+    annotator = Seafoam::Annotators::FallbackAnnotator.new
+    annotator.annotate graph
+    expect(node.props[:label]).to eq 'foo'
+  end
+
+  it 'does not overwrite an existing label annotation' do
+    graph = Seafoam::Graph.new
+    node = graph.create_node(0, {'label' => 'foo', :label => 'bar'})
+    annotator = Seafoam::Annotators::FallbackAnnotator.new
+    annotator.annotate graph
+    expect(node.props[:label]).to eq 'bar'
+  end
+
+  it 'adds nothing when there is no label property' do
+    graph = Seafoam::Graph.new
+    node = graph.create_node(0, {'xlabel' => 'foo'})
+    annotator = Seafoam::Annotators::FallbackAnnotator.new
+    annotator.annotate graph
+    expect(node.props[:label]).to be nil
+  end
+end
