8edb736c3b4c2733d50dc3e82560cfda747d918b
Chris Seaton <chris.seaton@shopify.com>
2020-11-23 15:55:08 +0000

    Sketch of cfg2asm

--- lib/seafoam.rb
+++ lib/seafoam.rb
@@ -1,6 +1,8 @@
 require 'seafoam/version'
 require 'seafoam/binary/io_binary_reader'
 require 'seafoam/bgv/bgv_parser'
+require 'seafoam/cfg/cfg_parser'
+require 'seafoam/cfg/disassembler'
 require 'seafoam/colors'
 require 'seafoam/graph'
 require 'seafoam/annotators'
