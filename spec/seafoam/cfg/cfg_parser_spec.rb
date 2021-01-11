7cc8bb6dd7b351cd70adc8837c41575d5897ee8d
Lillian Zhang <lillian.zhang@shopify.com>
2021-01-11 14:17:14 -0500

    suggestions from code review

--- spec/seafoam/cfg/cfg_parser_spec.rb
+++ spec/seafoam/cfg/cfg_parser_spec.rb
@@ -9,14 +9,8 @@ describe Seafoam::CFG::CFGParser do
     @example_cfg = File.expand_path('../../../examples/java/exampleIf.cfg', __dir__)
   end
 
-  it 'can skip to an nmethod in a file' do
-    parser = Seafoam::CFG::CFGParser.new(@example_cfg)
-    parser.skip_over_cfg 'After code installation'
-    parser.read_nmethod
-  end
-
   it 'correctly parses information from an nmethod' do
-    parser = Seafoam::CFG::CFGParser.new(@example_cfg)
+    parser = Seafoam::CFG::CFGParser.new($stdout, @example_cfg)
     parser.skip_over_cfg 'After code installation'
     nmethod = parser.read_nmethod
     expect(nmethod[:code][:base]).to eq '112e35c20'.to_i(16)
