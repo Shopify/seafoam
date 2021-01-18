98a4cbe0f5bdbcff5b3ad1d7885386439f51ec5d
Chris Seaton <chris.seaton@shopify.com>
2021-01-18 17:42:39 +0000

    Handle NaN in graphs when outputting as JSON

---
+++ spec/seafoam/json_writer_spec.rb
@@ -0,0 +1,14 @@
+require 'seafoam'
+
+require 'rspec'
+
+require_relative 'spec_helpers'
+
+describe Seafoam::JSONWriter do
+  describe '#prepare_json' do
+    it 'transforms a NaN to the string [NaN]' do
+      writer = Seafoam::JSONWriter.new(nil)
+      expect(writer.send(:prepare_json, { a: 1.0, b: Float::NAN, c: 2.0 })).to eq({ a: 1.0, b: '[NaN]', c: 2.0 })
+    end
+  end
+end
