cdbe2da1d3df6b80d4cd6fbc2411687bd5d1b62a
Kevin Menard <kevin.menard@shopify.com>
2024-03-15 11:38:45 -0400

    Overhaul the example generator.
    * Run the Ruby examples on GraalVM distributions that support it.
    * Added GraalVM 22.3.1 to the list of GraalVM distributions used for graph generation.
    * Restructure the way we run commands to enable code reuse.

--- tools/generate-examples.rb
+++ tools/generate-examples.rb
@@ -1,3 +1,7 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'fileutils'
 require 'pathname'
 
 GraalVM = Struct.new(:url, :dir) do
@@ -11,10 +15,88 @@ GraalVM = Struct.new(:url, :dir) do
 end
 
 GraalVM_21_2_0 = GraalVM.new('https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-21.2.0/graalvm-ce-java11-darwin-amd64-21.2.0.tar.gz', 'graalvm-ce-java11-21.2.0')
+GraalVM_22_3_1 = GraalVM.new('https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.1/graalvm-ce-java17-darwin-amd64-22.3.1.tar.gz', 'graalvm-ce-java17-22.3.1')
 
 GRAAL_VMS = [
+  GraalVM_22_3_1,
   GraalVM_21_2_0
 ]
+
+def run_java(bin, dump_level, *args)
+  FileUtils.rm_rf('./graal_dumps')
+
+  pipe = IO.popen(["#{bin}/java", \
+                   '-XX:+PrintCompilation', \
+                   '-Dgraal.PrintGraphWithSchedule=true', \
+                   "-Dgraal.Dump=:#{dump_level}"] + args)
+
+  yield pipe
+
+  sleep 3
+  Process.kill 'KILL', pipe.pid
+  Process.wait pipe.pid
+end
+
+def run_js(bin, *args)
+  FileUtils.rm_rf('graal_dumps')
+
+  pipe = IO.popen(["#{bin}/node", '--experimental-options', \
+                   '--engine.CompileOnly=fib', '--engine.TraceCompilation', \
+                   '--engine.MultiTier=false', '--engine.Inlining=false', \
+                   '--vm.Dgraal.Dump=Truffle:1'] + args, { err: %i[child out] })
+
+  yield pipe
+
+  sleep 3
+  Process.kill 'KILL', pipe.pid
+  Process.wait pipe.pid
+end
+
+def run_ruby(bin, *args)
+  FileUtils.rm_rf('./graal_dumps')
+
+  pipe = IO.popen(["#{bin}/ruby", '--jvm', '--experimental-options', \
+                   '--engine.TraceCompilation', \
+                   '--engine.NodeSourcePositions', '--engine.MultiTier=false', \
+                   '--engine.Inlining=false', '--vm.Dgraal.Dump=Truffle:1'] + args, { err: %i[child out] })
+
+  yield pipe
+
+  sleep 3
+  Process.kill 'KILL', pipe.pid
+  Process.wait pipe.pid
+end
+
+def process_examples(graalvm, language, pattern)
+  Dir.mkdir language unless Dir.exist?(language)
+  Dir.mkdir "../../examples/#{graalvm.name}/#{language}" unless Dir.exist?("../../examples/#{graalvm.name}/#{language}")
+  Dir.glob "graal_dumps/**/*\\[#{Regexp.escape(pattern)}example*.bgv" do |bgv_file|
+    next if bgv_file.end_with?('_1.bgv') # The AST graphs have the same name as the primary graph with a '_1' suffix. Skip them.
+    bgv_file =~ /HotSpotCompilation-\d+\[#{Regexp.escape(pattern)}(.+?)[\(\]]/
+    method = Regexp.last_match(1)
+
+    system 'cp', bgv_file, "#{method}.bgv"
+
+    if language == 'java'
+      system 'gzip', '-f', "#{method}.bgv"
+      system 'cp', "#{method}.bgv.gz", "../../examples/#{graalvm.name}/#{language}"
+    else
+      system 'cp', "#{method}.bgv", "../../examples/#{graalvm.name}/#{language}"
+    end
+
+    if graalvm == GRAAL_VMS.first
+      if language == 'java'
+        system 'cp', "#{method}.bgv.gz", "../../examples/#{language}"
+      else
+        system 'cp', "#{method}.bgv", "../../examples/#{language}"
+      end
+    end
+  end
+end
+
+def log(graalvm, message)
+  puts "#{graalvm.name}: #{message}"
+end
 
 Dir.chdir 'tools' do
   GRAAL_VMS.each do |graalvm|
@@ -25,24 +107,30 @@ Dir.chdir 'tools' do
 
       bin = "#{graalvm.dir}/Contents/Home/bin"
 
+      log(graalvm, "Installing Truffle languages.")
+
       unless Dir.exist?(graalvm.dir)
         system 'tar', '-zxf', "../#{graalvm.tarball}"
         system "#{bin}/gu", 'install', 'nodejs'
         system "#{bin}/gu", 'install', 'ruby'
       end
 
+
+      ###########################
+      ##### Java: Fibonacci #####
+      ###########################
+
+      log(graalvm, "Running Fibonacci in Java.")
+
       system 'cp', '../../examples/Fib.java', '.'
       system "#{bin}/javac", 'Fib.java'
 
-      system 'rm', '-rf', 'graal_dumps'
-      pipe = IO.popen(["#{bin}/java", '-XX:CompileOnly=Fib::fib', '-XX:+PrintCompilation', '-Dgraal.Dump=:1', '-Dgraal.PrintGraphWithSchedule=true', 'Fib', '14'])
-      loop do
-        line = pipe.gets
-        break if line =~ /4\s+Fib::fib/
+      run_java(bin, 1, '-XX:CompileOnly=Fib::fib', 'Fib', '14') do |pipe|
+        loop do
+          line = pipe.gets
+          break if line =~ /4\s+Fib::fib/
+        end
       end
-      sleep 3
-      Process.kill 'KILL', pipe.pid
-      Process.wait pipe.pid
 
       bgv = Dir.glob('graal_dumps/**/*.bgv')
       raise unless bgv.size == 1
@@ -55,50 +143,40 @@ Dir.chdir 'tools' do
         system 'cp', 'fib-java.bgv.gz', '../../examples'
       end
 
+
+
+      ###########################
+      ##### Java: Examples  #####
+      ###########################
+
+      log(graalvm, "Running Java examples.")
+
       system 'cp', '../../examples/java/JavaExamples.java', '.'
       system "#{bin}/javac", 'JavaExamples.java'
 
-      system 'rm', '-rf', 'graal_dumps'
-      pipe = IO.popen(["#{bin}/java", '-XX:-UseOnStackReplacement', \
-                       '-XX:CompileCommand=dontinline,*::*', \
-                       '-XX:+PrintCompilation', \
-                       '-Dgraal.PrintGraphWithSchedule=true', \
-                       '-Dgraal.Dump=:3', 'JavaExamples'])
-      loop do
-        line = pipe.gets if select([pipe], nil, nil, 5)
-        break if line.nil? || line =~ /4\s+Fib::fib/
-      end
-      sleep 3
-      Process.kill 'KILL', pipe.pid
-      Process.wait pipe.pid
-
-      Dir.mkdir 'java' unless Dir.exist?('java')
-      Dir.mkdir "../../examples/#{graalvm.name}/java" unless Dir.exist?("../../examples/#{graalvm.name}/java")
-      Dir.glob 'graal_dumps/**/*\\[JavaExamples\\.example*.bgv' do |bgv_file|
-        bgv_file =~ /HotSpotCompilation-\d+\[JavaExamples\.(.+)\(/
-        method = Regexp.last_match(1)
-        system 'cp', bgv_file, "#{method}.bgv"
-        system 'gzip', '-f', "#{method}.bgv"
-        system 'cp', "#{method}.bgv.gz", "../../examples/#{graalvm.name}/java"
-
-        if graalvm == GRAAL_VMS.first
-          system 'cp', "#{method}.bgv.gz", '../../examples/java'
+      run_java(bin, 3, '-XX:-UseOnStackReplacement', '-XX:CompileCommand=dontinline,*::*', 'JavaExamples') do |pipe|
+        loop do
+          line = pipe.gets if select([pipe], nil, nil, 5)
+          break if line.nil? || line =~ /4\s+Fib::fib/
         end
       end
 
-      system 'rm', '-rf', 'graal_dumps'
-      pipe = IO.popen(["#{bin}/node", '--experimental-options', \
-                       '--engine.CompileOnly=fib', '--engine.TraceCompilation', \
-                       '--engine.MultiTier=false', '--engine.Inlining=false', \
-                       '--vm.Dgraal.Dump=Truffle:1', \
-                       '../../examples/fib.js', '14'], { err: %i[child out] })
-      loop do
-        line = pipe.gets
-        break if line =~ /opt done\s+fib/
+      process_examples(graalvm, 'java', 'JavaExamples.')
+
+
+
+      #################################
+      ##### JavaScript: Fibonacci #####
+      #################################
+
+      log(graalvm, "Running Fibonacci in JavaScript.")
+
+      run_js(bin, '../../examples/fib.js', '14') do |pipe|
+        loop do
+          line = pipe.gets
+          break if line =~ /opt done\s+(?:id=\d+\s+)?fib/
+        end
       end
-      sleep 3
-      Process.kill 'KILL', pipe.pid
-      Process.wait pipe.pid
 
       bgv = Dir.glob('graal_dumps/**/*_fib\\].bgv')
       bgv_ast = Dir.glob('graal_dumps/**/*\\[fib\\].bgv')
@@ -117,20 +195,20 @@ Dir.chdir 'tools' do
         system 'cp', 'fib-js-ast.bgv.gz', '../../examples/fib-js-ast.bgv.gz'
       end
 
-      system 'rm', '-rf', 'graal_dumps'
-      pipe = IO.popen(["#{bin}/ruby", '--jvm', '--experimental-options', \
-                       '--engine.CompileOnly=fib', '--engine.TraceCompilation', \
-                       '--engine.NodeSourcePositions', '--engine.MultiTier=false', \
-                       '--engine.Inlining=false', '--vm.Dgraal.Dump=Truffle:1', \
-                       '../../examples/fib.rb', \
-                       '14'], { err: %i[child out] })
-      loop do
-        line = pipe.gets
-        break if line =~ /opt done\s+Object#fib/
+
+
+      ###########################
+      ##### Ruby: Fibonacci #####
+      ###########################
+
+      log(graalvm, "Running Fibonacci in Ruby.")
+
+      run_ruby(bin, '--engine.CompileOnly=fib', '../../examples/fib.rb', '14') do |pipe|
+        loop do
+          line = pipe.gets
+          break if line =~ /opt done\s+(?:id=\d+\s+)?Object#fib/
+        end
       end
-      sleep 3
-      Process.kill 'KILL', pipe.pid
-      Process.wait pipe.pid
 
       bgv = Dir.glob('graal_dumps/**/*\\[Object#fib\\].bgv')
       bgv_ast = Dir.glob('graal_dumps/**/*\\[Object#fib\\]_1.bgv')
@@ -147,6 +225,26 @@ Dir.chdir 'tools' do
       if graalvm == GRAAL_VMS.first
         system 'cp', 'fib-ruby.bgv.gz', '../../examples'
         system 'cp', 'fib-ruby-ast.bgv.gz', '../../examples'
+      end
+
+
+
+      ###########################
+      ##### Ruby: Examples  #####
+      ###########################
+
+      # Older GraalVM instances did not support the `--engine.InlineOnly` option. We had to manually patch Graal to
+      # add the inlining support. This script does not manually build GraalVM. Rather, the older graphs were manually
+      # created and added to this repo. They can be retrieved via git.
+      unless graalvm == GraalVM_21_2_0
+        run_ruby(bin, '--engine.OSR=false', '--engine.InlineOnly=~Object#opaque_,~Object#static_call,~ExampleObject#instance_call', '../../examples/ruby/ruby_examples.rb') do |pipe|
+          loop do
+            line = pipe.gets if select([pipe], nil, nil, 5)
+            break if line.nil?
+          end
+        end
+
+        process_examples(graalvm, 'ruby', 'Object#')
       end
     end
   end
